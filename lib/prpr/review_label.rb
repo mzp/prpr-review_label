68449d06f77ce1bce32454658d8d1c79312327f5
mzp <mzpppp@gmail.com>
2015-09-05 13:33:28 +0900

    bundle gem prpr-review_label

---
+++ lib/prpr/review_label.rb
@@ -0,0 +1,7 @@
+require "prpr/review_label/version"
+
+module Prpr
+  module ReviewLabel
+    # Your code goes here...
+  end
+end
---
+++ lib/prpr/review_label/version.rb
@@ -0,0 +1,5 @@
+module Prpr
+  module ReviewLabel
+    VERSION = "0.1.0"
+  end
+end
