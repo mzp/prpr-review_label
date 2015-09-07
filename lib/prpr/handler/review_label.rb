dfa30d53b309cb5885189a407b78ac68c1069185
mzp <mzpppp@gmail.com>
2015-09-07 10:21:31 +0900

    fix query

--- lib/prpr/handler/review_label.rb
+++ lib/prpr/handler/review_label.rb
@@ -1,7 +1,8 @@
 module Prpr
   module Handler
     class ReviewLabel < Base
-      handle Event::PullRequest, action: /labeled/ do
+      handle Event::PullRequest, action: 'labeled' do
+        # REMARK: /labeled/ is matched with 'unlabeled'
         Prpr::Action::ReviewLabel::Labeled.new(event).call
       end
     end
